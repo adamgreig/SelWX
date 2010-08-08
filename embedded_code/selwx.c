bdfa16d57e6ffbf907388b1b064b5e10e92d5919
Adam Greig <random@randomskk.net>
2010-08-08 01:25:37 +0100

    Monitor rain and wind sensors and flash LEDs appropriately

--- embedded_code/selwx.c
+++ embedded_code/selwx.c
@@ -1,20 +1,6 @@
 /*
- * This file is part of the libopenstm32 project.
- *
- * Copyright (C) 2009 Uwe Hermann <uwe@hermann-uwe.de>
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * SelWX
+ * Autonomous weather station
  */
 
 #include <libopenstm32/rcc.h>
@@ -22,48 +8,47 @@
 
 void gpio_setup(void)
 {
-    /* Enable GPIOC clock. */
-    /* Manually: */
-    // RCC_APB2ENR |= IOPCEN;
-    /* Using API functions: */
-    rcc_peripheral_enable_clock(&RCC_APB2ENR, IOPCEN);
+    // Enable GPIOC peripheral clock
+    rcc_peripheral_enable_clock(&RCC_APB2ENR, RCC_APB2ENR_IOPCEN);
 
-    /* Set GPIO12 (in GPIO port C) to 'output push-pull'. */
-    /* Manually: */
-    // GPIOC_CRH = (GPIO_CNF_OUTPUT_PUSHPULL << (((12 - 8) * 4) + 2));
-    // GPIOC_CRH |= (GPIO_MODE_OUTPUT_2_MHZ << ((12 - 8) * 4));
-    /* Using API functions: */
-    gpio_set_mode(GPIOC, GPIO_MODE_OUTPUT_2_MHZ,
-              GPIO_CNF_OUTPUT_PUSHPULL, GPIO6);
-    gpio_set_mode(GPIOC, GPIO_MODE_OUTPUT_2_MHZ,
-              GPIO_CNF_OUTPUT_PUSHPULL, GPIO7);
+    // Set GPIO6 and 7 to outputs (LEDs)
+    gpio_set_mode(
+        GPIOC,
+        GPIO_MODE_OUTPUT_2_MHZ,
+        GPIO_CNF_OUTPUT_PUSHPULL,
+        GPIO6 | GPIO7
+    );
+
+    // Set GPIO11 and 12 to inputs
+    gpio_set_mode(
+        GPIOC,
+        GPIO_MODE_INPUT,
+        GPIO_CNF_INPUT_PULL_UPDOWN,
+        GPIO11 | GPIO12
+    );
+
+    // Set pullup resistors on GPIO11 and 12
+    gpio_set(GPIOC, GPIO11 | GPIO12);
 }
 
 int main(void)
 {
-    int i;
-
     gpio_setup();
 
-    /* Blink the LED (PC12) on the board. */
-    gpio_set(GPIOC, GPIO6);
     while (1) {
-        /* Manually: */
-        // GPIOC_BSRR = GPIO12;         /* LED off */
-        // for (i = 0; i < 80000; i++); /* Wait (needs -O0 CFLAGS). */
-        // GPIOC_BRR = GPIO12;          /* LED on */
-        // for (i = 0; i < 80000; i++); /* Wait (needs -O0 CFLAGS). */
+        // Read the wind speed and rain sensors, then write their
+        // status to the LEDs.
+        int weather = gpio_get(GPIOC, GPIO11 | GPIO12);
 
-        /* Using API functions gpio_set()/gpio_clear(): */
-        // gpio_set(GPIOC, GPIO12);     /* LED off */
-        // for (i = 0; i < 80000; i++); /* Wait (needs -O0 CFLAGS). */
-        // gpio_clear(GPIOC, GPIO12);   /* LED on */
-        // for (i = 0; i < 80000; i++); /* Wait (needs -O0 CFLAGS). */
+        if(weather & GPIO11)
+            gpio_clear(GPIOC, GPIO6);
+        else
+            gpio_set(GPIOC, GPIO6);
 
-        /* Using API function gpio_toggle(): */
-        gpio_toggle(GPIOC, GPIO6);     /* LED on/off */
-        gpio_toggle(GPIOC, GPIO7);
-        for (i = 0; i < 80000; i++);    /* Wait (needs -O0 CFLAGS). */
+        if(weather & GPIO12)
+            gpio_clear(GPIOC, GPIO7);
+        else
+            gpio_set(GPIOC, GPIO7);
     }
 
     return 0;
